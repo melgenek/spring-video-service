f732d0ab03c79aa8a9063ff27762ccf6e7035284
Yevhenii Melnyk <ext.emelnik@tinkoff.ru>
2018-05-06 14:45:11 +0300

    Download video

--- src/main/kotlin/sample/service/video/Application.kt
+++ src/main/kotlin/sample/service/video/Application.kt
@@ -3,9 +3,10 @@ package sample.service.video
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+	SpringApplication.run(Application::class.java, *args)
 }
---
+++ src/main/resources/templates/video.ftl
@@ -0,0 +1,3 @@
+<video width="320" height="240" autoplay="autoplay" controls>
+    <source src="/videos/${videoName}" type="video/mp4">
+</video>
